d19dc2bd00975b42a8edcab4ad8834662d96f4e9
Furk0v <furk0v@vk.com>
2024-10-16 23:41:30 +0300

    Add files via upload

---
+++ ClanDemonoff.ru/ClanDemonOFF.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="./css/clan.css" rel="stylesheet" />
+    <title>ClanDemonoff</title>
+</head>
+
+<body>
+    <div class="v1_2">
+        <div class="v1_11"></div>
+        <div class="v1_19">
+            <div class="v1_20"></div>
+            <div class="v84_108">
+                <div class="v84_109"></div>
+                <div class="v84_110">
+                    <div class="v84_111"></div>
+                    <span class="v84_112">VIPERR</span>
+                    <span class="v84_113">Heavy metal 2</span>
+                    <span class="v84_114">5</span>
+                    <div class="v84_115"></div>
+                    <div class="v106_16"></div>
+                </div>
+                <div class="v84_116">
+                    <div class="v84_117"></div>
+                    <span class="v84_118">Clan Demonoff</span>
+                    <span class="v84_119">Achievement 2</span>
+                    <span class="v84_120">4</span>
+                    <div class="v84_121"></div>
+                    <div class="v106_14"></div>
+                </div>
+                <div class="v84_122">
+                    <div class="v84_123"></div>
+                    <span class="v84_124">VIPERR</span>
+                    <span class="v84_125">Heavy metal 2</span>
+                    <span class="v84_126">3</span>
+                    <div class="v84_127"></div>
+                    <div class="v106_12"></div>
+                </div>
+                <div class="v84_128">
+                    <div class="v84_129"></div>
+                    <span class="v84_130">VIPERR</span>
+                    <span class="v84_131">Heavy metal 2</span>
+                    <span class="v84_132">2</span>
+                    <div class="v84_133"></div>
+                    <div class="v106_10"></div>
+                </div>
+                <div class="v84_134">
+                    <div class="v84_135"></div>
+                    <span class="v84_136">VIPERR</span>
+                    <span class="v84_137">Heavy metal 2</span>
+                    <span class="v84_138">1</span>
+                    <div class="v84_139">
+                        <div class="v84_140">
+                            <div class="v84_141"></div>
+                            <div class="v84_142"></div>
+                        </div>
+                        <div class="v84_143"></div>
+                        <div class="v84_144"></div>
+                        <span class="v84_145">2</span>
+                    </div>
+                    <div class="v84_146"></div>
+                    <div class="v106_8"></div>
+                </div>
+                <span class="v84_147">Пот худших</span>
+            </div>
+            <div class="v74_2">
+                <div class="v45_19"></div>
+                <div class="v48_4">
+                    <div class="v48_3"></div>
+                    <span class="v74_30">VIPERR</span>
+                    <span class="v45_25">Heavy metal 2</span>
+                    <span class="v52_2">5</span>
+                    <div class="v74_29"></div>
+                    <div class="v106_6"></div>
+                </div>
+                <div class="v74_59">
+                    <div class="v74_60"></div>
+                    <span class="v74_61">VIPERR</span>
+                    <span class="v74_62">Heavy metal 2</span>
+                    <span class="v74_63">4</span>
+                    <div class="v74_64"></div>
+                    <div class="v106_4"></div>
+                </div>
+                <div class="v74_52">
+                    <div class="v74_53"></div>
+                    <span class="v74_54">VIPERR</span>
+                    <span class="v74_55">Heavy metal 2</span>
+                    <span class="v74_56">3</span>
+                    <div class="v74_57"></div>
+                    <div class="v106_2"></div>
+                </div>
+                <div class="v74_45">
+                    <div class="v74_46">
+                    </div>
+                    <span class="v74_47">VIPERR</span>
+                    <span class="v74_48">Heavy metal 2</span>
+                    <span class="v74_49">2</span>
+                    <div class="v74_50"></div>
+                    <div class="v99_2"></div>
+                </div>
+                <div class="v74_32">
+                    <div class="v74_33"></div>
+                    <span class="v74_34">VIPERR</span>
+                    <span class="v74_35">Heavy metal 2</span>
+                    <span class="v74_36">1</span>
+                    <div class="v74_37">
+                        <div class="v81_87">
+                            <div class="v74_38"></div>
+                            <div class="v74_39"></div>
+                        </div>
+                        <div class="v74_40"></div>
+                        <div class="v74_41"></div>
+                        <span class="v74_42">2</span>
+                    </div>
+                    <div class="v74_43"></div>
+                    <div class="v99_3"></div>
+                </div>
+                <span class="v57_4">Пот новинок</span>
+            </div>
+            <div class="v74_86">
+                <div class="v74_28"></div>
+                <div class="v81_88">
+                    <div class="v74_76">
+                        <div class="name"></div>
+                        <div class="v74_78"></div>
+                        <div class="v74_79"></div>
+                        <span class="v74_80">Пара долго пыталась скрывать отношения из-за того, что Дилара устала от
+                            публичности своих прошлых отношений с иноагентом Алишером</span>
+                        <span class="v74_81">Дилара и Бустер объявили о том, что встречаются</span>
+                        <span class="v74_82">1 апреля</span>
+                        <span class="v74_83">KOSTI BLOGGERS</span>
+                        <div class="v74_84"></div>
+                    </div>
+                    <div class="v81_89">
+                        <div class="name"></div>
+                        <div class="v81_91"></div>
+                        <div class="v81_92"></div>
+                        <span class="v81_93">Пара долго пыталась скрывать отношения из-за того, что Дилара устала от
+                            публичности своих прошлых отношений с иноагентом Алишером</span>
+                        <span class="v81_94">Дилара и Бустер объявили о том, что встречаются</span>
+                        <span class="v81_95">1 апреля</span>
+                        <span class="v81_96">KOSTI BLOGGERS</span>
+                        <div class="v81_97"></div>
+                    </div>
+                </div>
+                <div class="v84_107">
+                    <div class="v81_99"></div>
+                    <div class="v81_102">
+                        <div class="v81_100"></div>
+                        <span class="v81_101">Рецензии</span>
+                    </div>
+                    <div class="v81_103">
+                        <div class="v81_104"></div>
+                        <span class="v81_105">Новости</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="v27_3">
+            <div class="v1_23"></div>
+            <div class="v16_2">
+                <div class="v16_3"></div>
+                <span class="v16_4">FA Q</span>
+            </div>
+            <div class="v17_2">
+                <div class="v17_3"></div>
+                <span class="v17_4">Поты треков</span>
+            </div>
+            <div class="v14_3">
+                <div class="v14_2"></div>
+                <span class="v13_2">Главная</span>
+            </div>
+        </div>
+        <div class="v23_2">
+            <div class="v23_3"></div>
+            <div class="name"></div>
+            <div class="name"></div>
+        </div>
+        <div class="v1_17">
+            <div class="v1_6"></div>
+            <div class="name"></div>
+            <div class="name"></div>
+        </div>
+        <div class="v91_156">
+            <div class="v91_154"></div>
+            <div class="v1_16">
+                <div class="v1_13"></div>
+                <span class="v1_15">Поиск треков</span>
+            </div>
+            <div class="v88_151"></div>
+        </div>
+        <span class="v1_12">Clan</span>
+    </div>
+</body>
+
+</html>
